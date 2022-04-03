fca2b01fb27f2a902bb4954e2fce7a0e347f30f5
kritinidzin <danil.kritsin@gmail.com>
2022-04-03 22:30:04 +0300

    Добавлено открытые заявки шаблон

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,22 +1,34 @@
 from aiogram import Dispatcher, types
 from dataBase.execute_query import execute_query, paste_user
 from create_bot import db_name
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
 
 from create_bot import dp, bot
-from keyboards import btn_case_admin
+from keyboards import btn_case_admin, inline_btn_block_control
+
+class auth_adm(StatesGroup):
+    id = State()
 
 #  __________________________старт_____________________________
 
-async def command_start(message : types.Message):
-    try:
-        await bot.send_message(message.from_user.id, 'Hello, I was created to introduce you to telegram-bots. If you see this message, you have started a dialogue with me, and here can be any message that you want to show to anyone who starts a dialogue with me\n\nЗдравствуйте, я создан, чтобы познакомить вас с телеграм-ботами. Если вы видите это сообщение, вы начали диалог со мной, и здесь может быть любое сообщение, которое вы хотите показать всем, кто начинает диалог со мной.', reply_markup=btn_case_admin)
-    except:
-        await message.answer('Something went wrong, error messages are processed by me as quickly as possible\n\nЧто-то пошло не так, сообщения об ошибках обрабатываются мной максимально быстро')
+# async def command_start(message : types.Message):
+#     try:
+#         await bot.send_message(message.from_user.id, 'Hello, I was created to introduce you to telegram-bots. If you see this message, you have started a dialogue with me, and here can be any message that you want to show to anyone who starts a dialogue with me\n\nЗдравствуйте, я создан, чтобы познакомить вас с телеграм-ботами. Если вы видите это сообщение, вы начали диалог со мной, и здесь может быть любое сообщение, которое вы хотите показать всем, кто начинает диалог со мной.', reply_markup=btn_case_admin)
+#     except:
+#         await message.answer('Something went wrong, error messages are processed by me as quickly as possible\n\nЧто-то пошло не так, сообщения об ошибках обрабатываются мной максимально быстро')
+async def auth_admin(message : types.Message):
+    if (message.from_user.id == 995404025):
+        await message.answer("admin auth", reply_markup=btn_case_admin)
 
+async def load_requests(message : types.Message):
+    await message.answer("Load method", reply_markup=inline_btn_block_control)
 
+async def remove_requests(message : types.Message):
+    await message.answer("Remove method")
 
-def register_handlers_client(dp: Dispatcher):
+def register_handlers_admin(dp: Dispatcher):
     offer=lambda c: c.data.find("Offer") > 0
-    dp.register_message_handler(command_start, commands=['load requests'])
-    dp.register_message_handler(command_start, commands=['remove requests'])
-    dp.register_callback_query_handler(command_start, offer)
+    dp.register_message_handler(load_requests, commands=['load_requests'])
+    dp.register_message_handler(remove_requests, commands=['remove_requests'])
+    dp.register_message_handler(auth_admin, commands=['auth'])
--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from create_bot import dp
 async def on_stratup(_):
     print('Bot was initialized')
 
-from handlers import client, other
+from handlers import client, other, admin
 
+admin.register_handlers_admin(dp)
 client.register_handlers_client(dp)
 other.register_handlers_other(dp)
+
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_stratup)
